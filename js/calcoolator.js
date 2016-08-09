6adae414f6c601a1c6271610a5aac5c463157ae1
Avand Amiri <avand@avandamiri.com>
2016-08-08 17:42:36 -0700

    Adding calculation function and UI changes

--- js/calcoolator.js
+++ js/calcoolator.js
@@ -1,10 +1,49 @@
+function calcPounds() {
+  var numberOfDays = parseInt($("#number-of-days input").val());
+  var totalFreezerQuarts = 0;
+  var totalFridgeQuarts = 0;
+
+  $(".cooler-type-active").each(function(i, activeCoolerType) {
+    activeCoolerType = $(activeCoolerType);
+    var quarts = parseInt(activeCoolerType.parents(".cooler").find("input").val());
+    if (activeCoolerType.data("cooler-type") == "freezer") {
+      totalFreezerQuarts += quarts;
+    } else {
+      totalFridgeQuarts += quarts;
+    }
+  });
+
+  var totalFreezerPounds = Math.ceil((totalFreezerQuarts / 3) * Math.pow(numberOfDays, 1.2));
+  var totalFridgePounds = Math.ceil((totalFridgeQuarts / 3) * Math.pow(numberOfDays, 1.1));
+
+  return {
+    days: numberOfDays,
+    freezer: {
+      totalQuarts: totalFreezerQuarts,
+      poundsDryIce: totalFreezerPounds
+    },
+    fridge: {
+      totalQuarts: totalFridgeQuarts,
+      poundsDryIce: totalFridgePounds
+    }
+  }
+}
+
+function updatePounds() {
+  var result = calcPounds();
+  var totalPounds = result.freezer.poundsDryIce + result.fridge.poundsDryIce;
+  $("#pounds").text(totalPounds);
+}
+
 $(document).ready(function() {
   $("#add-cooler-button").click(function(e) {
     e.preventDefault();
 
     var lastCooler = $(".cooler:last");
     lastCooler.clone().insertAfter(lastCooler);
+
     bindCoolerEvents();
+    updatePounds();
   })
 
   function bindCoolerEvents() {
@@ -13,6 +52,8 @@ $(document).ready(function() {
       var cooler = coolerType.parents(".cooler");
       cooler.find(".cooler-type").removeClass("cooler-type-active");
       coolerType.addClass("cooler-type-active");
+
+      updatePounds();
     })
 
     $(".cooler-remove-control").click(function(e) {
@@ -24,6 +65,8 @@ $(document).ready(function() {
       setTimeout(function() {
         cooler.remove();
       }, parseFloat(cooler.css("transition-duration")) * 1000);
+
+      updatePounds();
     })
 
     $(".cool-slider").on("input", function() {
@@ -31,13 +74,17 @@ $(document).ready(function() {
       var changes = slider.data("changes");
       var changesWithin = slider.data("changes-within");
       var val = slider.val();
+
       if (changesWithin) {
-        slider.parents(changesWithin).find(changes).text(val)
+        slider.parents(changesWithin).find(changes).text(val);
       } else {
-        $(changes).text(val)
+        $(changes).text(val);
       }
+
+      updatePounds();
     })
   }
 
   bindCoolerEvents();
+  updatePounds();
 })
